e1f37a951919427316d316bd589b26196aae7bea
Saran-R13 <saranyogi13022002@gmail.com>
2026-03-03 18:31:23 +0530

    just sql

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
+# save this as app.py
 from flask import Flask
+from routes.movies_bp import movies_bp
+from config import Config
+from extensions import db
+from sqlalchemy.sql import text
 
 app = Flask(__name__)
+app.config.from_object(Config)  # URL
+
+db.init_app(app)
 
 
-@app.route("/")
+with app.app_context():
+    try:
+        result = db.session.execute(text("SELECT 1")).fetchall()
+        print("Connection successful:", result)
+    except Exception as e:
+        print("Error connecting to the database:", e)
+
+
+# @app.get("/movies")
+# def movie():
+#     return movies
+
+
+@app.get("/")
 def hello():
-    return "<h1>Hello, World! 🎉 🔥</h1>"
+    return "<h1>Hello, World!  ROUND 👶 🎉 🔥</h1>"
+
+
+app.register_blueprint(movies_bp, url_prefix="/api/movies")
